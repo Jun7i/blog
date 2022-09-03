6b6c770473c28234ffe72e57a30d220f16c46e6e
Jun 7i <44311497+Jun7i@users.noreply.github.com>
2022-09-03 01:21:01 -0700

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,6 +11,7 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/', icon: 'reco-home' },
       { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+      { text: 'About Me', link: '/tags/'},
       { text: 'Docs', 
         icon: 'reco-message',
         items: [
